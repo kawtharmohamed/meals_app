726a1d1a64456f96117cacf6da2c1b682b97a4f6
kawtharmohamed <96089519+kawtharmohamed@users.noreply.github.com>
2023-11-20 09:44:10 -0800

    commit finished

--- lib/core/widgets/snack_bar.dart
+++ lib/core/widgets/snack_bar.dart
@@ -6,13 +6,14 @@ import 'package:meals_app/lang/locale_keys.g.dart';
 class SnackBarMessage {
   void showSuccessSnackBar(
       {required BuildContext context}) {
+    final isDarkMode = Theme.of(context).brightness== Brightness.dark;
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar( duration: Duration(seconds: 2),
         content: Text(
           LocaleKeys.Add.tr(),
           style: TextStyle(color: MyColors.White),
         ),
-        backgroundColor:MyColors.darkYellow,
+        backgroundColor: isDarkMode ? MyColors.Black : MyColors.darkYellow,
        shape: const RoundedRectangleBorder(
          borderRadius: BorderRadius.vertical(
            top: Radius.circular(15),
